4f1c8f573afd2a1f827a3130976b81ed00fd7ff9
rashmi0710 <dongrerashmi15@gmail.com>
2025-10-22 12:32:06 +0530

    how to make custom package

---
+++ 5-Modules/package/__inti__.py
@@ -0,0 +1,9 @@
+# # it is special file used in Python to define packages and initialize their namespaces 
+# ?
+
+# 👉 A package is just a folder that contains multiple Python files (modules).
+# But Python ko kaise pata chalega ke ye folder ek package hai, na ki normal folder?
+
+# The file __init__.py tells Python:
+
+# “Hey! This folder is a Python package. You can import from it.”
---
+++ 5-Modules/test.py
@@ -0,0 +1,4 @@
+from package.maths import *
+
+print(addition(2,3))
+print(substraction(4,3))
